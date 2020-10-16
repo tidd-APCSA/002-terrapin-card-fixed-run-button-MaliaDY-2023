9fb3746282f56060db093e7d3cbf54efee3a4e17
Malia Dy <662772@aacps.org>
2020-10-16 03:08:39 +0000

    Tested and Completed task 3

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -2,6 +2,11 @@ public class CardRunner {
     public static void main(String args[]){
         // ☕
         System.out.println("hey");
-        TerrapinCard card = new TerrapinCard(50);
+        TerrapinCard card = new TerrapinCard(5);
+        System.out.println(card);
+        card.payGourmet();
+        System.out.println(card);
+        card.payGourmet();
+        System.out.println(card);
     }
 }
