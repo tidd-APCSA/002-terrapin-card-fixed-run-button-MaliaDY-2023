3f5ab8e96e398f76e139121218633ff8dfa3db9d
Malia Dy <662772@aacps.org>
2020-10-16 02:39:22 +0000

    Tested Exercise 1

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -2,6 +2,6 @@ public class CardRunner {
     public static void main(String args[]){
         // ☕
         System.out.println("hey");
-
+        TerrapinCard card = new TerrapinCard(50);System.out.println(card);
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,32 @@
 public class TerrapinCard {
 
+private double balance;
 
+public TerrapinCard(double balanceAtStart)
+{
+  balance = balanceAtStart;
+}
+
+public String toString()
+{
+  return "The card has " + balance + " dollars";
+}
+
+public void payEconomical()
+{
+  if(balance < 2.5){
+    this.balance = balance;
+  }else{
+    balance -= 2.5;
+  }
+}
+
+public void payGourmet()
+{
+  if(balance < 4.0){
+    this.balance = balance;
+  }else{
+  balance -= 4.0;
+  }
+}
 }
