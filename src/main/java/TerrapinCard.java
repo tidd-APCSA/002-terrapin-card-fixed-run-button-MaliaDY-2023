4a585870bd39f46858a782ae1fe5cbcc4549ee6a
Malia Dy <662772@aacps.org>
2020-10-16 02:58:25 +0000

    Testing task 2

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -12,4 +12,14 @@ public String toString()
   return "The card has " + balance + " dollars";
 }
 
+public void payEconomical()
+{
+    balance -= 2.5;
+}
+
+public void payGourmet()
+{
+  balance -= 4.0;
+
+}
 }
